c5f48a5898e3ffd2fb51941180ccda58a58bc37e
HamsBoukhari <107585348+HamsBoukhari@users.noreply.github.com>
2024-05-07 00:47:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
-import torch
 import re
-import xml.etree.ElementTree as ET 
+import xmltodict
 
 @st.cache(allow_output_mutation=True)
 def get_model():
@@ -12,13 +11,14 @@ def get_model():
     return tokenizer,model
 
 tokenizer,model = get_model()
+st.title('Full Service')
 trade_msg = st.text_area('Trade Message')
 sgw_op = st.text_area('SGW Operation')
 button = st.button("Predict")
 
 if (trade_msg and sgw_op) and button:
-    trade_msg1 = trade_msg.replace('/n','')
-    sgw_op1 = sgw_op.replace('/n','')
+    trade_msg1 = trade_msg.replace('\n','')
+    sgw_op1 = sgw_op.replace('\n','')
     input_seq = trade_msg1+' '+sgw_op1
     pattern = r'[<\"/]'
     cleaned_seq1 = re.sub(pattern, '', input_seq)
@@ -28,22 +28,18 @@ if (trade_msg and sgw_op) and button:
     output = model.generate(input_ids, max_length=1000, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
     decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
     output_sequence = decoded_output[len(seq):].strip()
-    root1 = ET.fromstring(trade_msg)
-    root2 = ET.fromstring(output_sequence)
-    match_id_value1 = root1.attrib.get('MtchID')
-    match_id_value2 = root2.attrib.get('MtchID')
+    trade_msg_dict = xmltodict.parse(trade_msg)
+    output_sequence_dict = xmltodict.parse(output_sequence)
+    match_id_value1 = trade_msg_dict['TrdCaptRpt']['@MtchID']
+    match_id_value2 = output_sequence_dict['TrdCaptRpt']['@MtchID']
     if match_id_value1 != match_id_value2:
-       root2.set('MtchID', match_id_value1)
-    trd_id_value1 = root1.attrib.get('TrdID')
-    trd_id_value2 = root2.attrib.get('TrdID')
+        output_sequence_dict['TrdCaptRpt']['@MtchID'] = match_id_value1
+    trd_id_value1 = trade_msg_dict['TrdCaptRpt']['@TrdID']
+    trd_id_value2 = output_sequence_dict['TrdCaptRpt']['@TrdID']
     if trd_id_value1 != trd_id_value2:
-       root2.set('TrdID', trd_id_value1)
-    root3 = ET.fromstring(sgw_op)
-    pty_id_value = 0
-    for pty_element in root3.findall(".//Pty[@R='24']"):
-       pty_id_value = pty_element.attrib.get('ID')
-    if pty_id_value != 0:
-       for pty_element in root2.findall(".//Pty[@R='24']"):
-          pty_element.attrib['ID'] = pty_id_value
-    modified_ccp_message = ET.tostring(root2, encoding='unicode')
+        output_sequence_dict['TrdCaptRpt']['@TrdID'] = trd_id_value1
+    modified_ccp_message = xmltodict.unparse(output_sequence_dict)
+    if modified_ccp_message.startswith("<?xml"):
+        modified_ccp_message = modified_ccp_message.split("?>", 1)[1].strip()
+    modified_ccp_message = modified_ccp_message.replace("\n", "").replace("\t", "")
     st.write("CCP Message: ",modified_ccp_message)
